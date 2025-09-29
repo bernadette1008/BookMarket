168e14a359826385bd6d3d704510d397fc824d92
bernadette1008 <vachiv@naver.com>
2025-09-29 11:56:10 +0900

    BookMarket의 주문처리 1 - entity, repository, service, orderCustomerInfo.html

---
+++ src/main/java/kr/ac/kopo/yj/bookmarket/domain/Address.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.yj.bookmarket.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Address {
+    @Id
+    private int id;
+    private String country; //국가명
+    private String zipcode; //우편번호
+    private String addressName; //기본 주소
+    private String detailName; //상세 주소
+}
---
+++ src/main/java/kr/ac/kopo/yj/bookmarket/service/OrderService.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.yj.bookmarket.service;
+
+
+import kr.ac.kopo.yj.bookmarket.domain.Order;
+
+public interface OrderService {
+    void confirmOrder(String bookId, long quantity); // 도서 재고, 수량 확인
+    Long saveOrder(Order order);
+
+}
